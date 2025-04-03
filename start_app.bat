3d7147185629ff513289697cbc56fddd83c89f64
Thomas Kirschner <dev@tk-mk.de>
2025-04-03 12:23:02 +0200

    Update rename Class

--- start_app.bat
+++ start_app.bat
@@ -1,5 +1,5 @@
 @echo off
 echo Starte Zeitreihen-Visualisierungs-App...
 echo Lassen Sie den Terminal offen, hier bekommen Sie die Meldungen der App angezeigt.
-zeitreihen_venv\Scripts\python.exe -W ignore app_3_start.py
+zeitreihen_venv\Scripts\python.exe -W ignore StartApplication.py
 pause
